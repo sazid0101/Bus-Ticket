df9b22a6a6d08b7c06e1eca70ce399d12594b7cd
sazid0101 <shahriarislamsazid@gmail.com>
2025-02-12 18:35:19 +0600

    selected seat & its price show in the seat section by appendchild

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,10 +1,37 @@
+//when seat clicks seat number will increase
 const mySeat = document.getElementsByClassName("seat")
 let selectedSeat = 0;
 
 for(const seat of mySeat){
     seat.addEventListener("click", function(event){
        selectedSeat++
-       const seatCount = document.getElementById("seat-count")
-       seatCount.innerText = selectedSeat ;
+
+       const seatName = event.target.innerText ;
+       const seatPrice = "550 tk" ;
+       console.log(seatName)
+       console.log(seatPrice)
+       const selectedContainer = document.getElementById("selected-seat-container");
+
+       const li = document.createElement("li");
+       li.style.display = "flex";
+        li.style.justifyContent = "space-between";
+
+       const p = document.createElement("p");
+       p.innerText = seatName;
+
+       const p2 = document.createElement("p");
+       p2.innerText = seatPrice;
+
+       li.appendChild(p)
+       li.appendChild(p2)
+
+       selectedContainer.appendChild(li)
+       
+
+       setInnerText("seat-count", selectedSeat);  //function call, id & value
     })
+}
+
+function setInnerText(id,value){
+    document.getElementById(id).innerText = value ;  //this function is for set inner text.
 }
